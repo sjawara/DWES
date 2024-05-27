7d8bdc7b029e9bde651203d5977d783f8ad08195
sjawara <sjawara@institutmvm.cat>
2024-05-27 18:00:05 +0200

    Incicio de traspaso con hibernate

--- UF4_RESTFULL/src/main/java/com/mycompany/uf4_restfull/domain/Cliente.java
+++ UF4_RESTFULL/src/main/java/com/mycompany/uf4_restfull/domain/Cliente.java
@@ -4,14 +4,20 @@
  */
 package com.mycompany.uf4_restfull.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
-
+@Entity
 @XmlRootElement(name="cliente")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Cliente {
+    	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre;
     private String apellidos;
